f0e6a6a6304e2fec2d0523ed12b528b01642cc41
LinaCoGo <lina.tamara.contreras@gmail.com>
2021-02-11 15:45:37 +0000

    Program that prints numberts from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_numbers - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+
+	int num = '0';
+
+	while (num <= '9')
+	{
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}
