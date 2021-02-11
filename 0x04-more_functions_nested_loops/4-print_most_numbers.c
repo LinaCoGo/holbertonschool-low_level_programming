6eb1f51a48ace404654c466da20e1573d8f57671
LinaCoGo <lina.tamara.contreras@gmail.com>
2021-02-11 16:25:04 +0000

    Used for instead of the previous while

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,17 +7,12 @@
 void print_most_numbers(void)
 {
 
-	int num = '0';
+	int num;
 
-	while (num <= '9')
+	for (num = '0'; num <= '9'; num++)
 	{
 		if (num != '2' && num != '4')
-		{
 			_putchar(num);
-			num++;
-		}
-		else
-			num++;
 	}
 	_putchar('\n');
 }
