a71d1036d1314bf386bef5a86058f8e7ac1c0f69
LinaCoGo <lina.tamara.contreras@gmail.com>
2021-02-22 00:46:17 +0000

    Use other variables to move

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * cap_string - Capitalizes every word of a string.
+ * @s: Given string.
+ * Return: char s.
+ */
+char *cap_string(char *s)
+{
+
+	int i = 0;
+	int c = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == '\t')
+		{
+			s[i] = ' ';
+		}
+		if (s[i] == ' ' || s[i] == '.' || s[i] == '\n')
+		{
+			c = i + 1;
+			if (s[c] >= 'a' && s[c] <= 'z')
+			{
+				s[c] = s[c] - 32;
+			}
+		}
+	}
+	return (s);
+}
