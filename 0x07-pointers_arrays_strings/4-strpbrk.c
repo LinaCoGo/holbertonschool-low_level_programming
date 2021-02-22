8d3eef02fbfb4ab949447f7c4ea1da5a331024c7
LinaCoGo <lina.tamara.contreras@gmail.com>
2021-02-22 22:51:54 +0000

    function that searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *_strpbrk - locates the first occurrence in s of any byte the string accept.
+ *@s: matches one of the bytes in another var.
+ *@accept: string pointer.
+ * Return: p.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return ('\0');
+}
